3018c1d3626d23ae0cd390ab83144e7f76605f76
bruno-soares-meerkat <brunolima280103@gmail.com>
2024-10-17 03:05:16 -0300

    fix: axios connect problem

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,37 +1,32 @@
-import axios from "axios";
-import queryString from "query-string";
+import axios from 'axios'
+import queryString from 'query-string'
 
-const baseUrl = "kanban-task-manager-api.vercel.app/";
-const getToken = () => localStorage.getItem("token");
+const baseUrl = "kanban-task-manager-api.vercel.app/api/v1";
+const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
-    baseURL: baseUrl,
-    paramsSerializer: params => queryString.stringify({params})
-});
+  baseURL: baseUrl,
+  paramsSerializer: params => queryString.stringify({ params })
+})
 
 axiosClient.interceptors.request.use(async config => {
-    const token = getToken();
-    if (token) {
-        config.headers.authorization = `Bearer ${token}`;
+  return {
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+      'authorization': `Bearer ${getToken()}`
     }
-    return {
-        ...config,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
-});
+  }
+})
 
 axiosClient.interceptors.response.use(response => {
-    if (response && response.data) return response.data;
-    return response;
+  if (response && response.data) return response.data
+  return response
 }, err => {
-    if (!err.response) {
-        console.log("Erro de conexão:", err);
-    } else {
-        console.log("Erro na resposta:", err.response);
-    }
-    throw err.response;
-});
+  if (!err.response) {
+    return alert(err)
+  }
+  throw err.response
+})
 
-export default axiosClient;
+export default axiosClient
